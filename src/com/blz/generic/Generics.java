8f7f60f0f65841a1f57d166e1f92163da65c872e
Priya <Priyamadiwalar0007@gmail.com>
2022-02-27 23:33:19 +0530

    Added:RefactorGenericMethod

--- src/com/blz/generic/Generics.java
+++ src/com/blz/generic/Generics.java
@@ -1,44 +1,22 @@
 package com.blz.generic;
 
 public class Generics<T extends Comparable<T>> {
-	public T getMax(T first, T second, T three) {
-		T max = first;
-		if (second.compareTo(max) > 0) {
-			max = second;
+	public static <T extends Comparable> T getMax(T first, T second, T third) {
+		if (first.compareTo(second) > 0 && first.compareTo(third) > 0) {
+			return first;
+		} else if (second.compareTo(first) > 0 && second.compareTo(third) > 0) {
+			return second;
+		} else {
+			return third;
 		}
-		if (three.compareTo(max) > 0) {
-			max = three;
-		}
-		printMax(first, second, three, max);
-		return max;
-	}
-
-	public static String getMaxStr(String a, String b, String c) {
-		String max = a;
-		if (b.compareTo(max) > 0) {
-			max = b;
-		}
-		if (c.compareTo(max) > 0) {
-			max = c;
-		}
-		printMax(a, b, c, max);
-		return max;
-	}
-
-	public static <T> void printMax(T a, T b, T c, T max) {
-		System.out.printf("Max of %s, %s and %s is %s\n", a, b, c, max);
 	}
 
 	public static void main(String[] args) {
 		Integer firstInt = 1, secondInt = 2, thirdInt = 3;
 		Float firstFloat = 1.2f, secondFloat = 8.2f, thirdFloat = 60.2f;
 		String firstString = "Jai", secondString = "Shree", thirdString = "Ram";
-
-		Generics<String> refactorGenericClass = new Generics();
-		Generics<Integer> refactorGenericClass1 = new Generics();
-		Generics<Float> refactorGenericClass11 = new Generics();
-
-		System.out.println(refactorGenericClass.getMax(firstString, secondString, thirdString));
-
+		System.out.println(getMax(firstInt, secondInt, thirdInt));
+		System.out.println(getMax(firstFloat, secondFloat, thirdFloat));
+		System.out.println(getMax(firstString, secondString, thirdString));
 	}
 }
