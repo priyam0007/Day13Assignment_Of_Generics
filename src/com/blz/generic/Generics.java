aa2fe4e8a0e30a0f77fb2cceb4aedae29207c4aa
Priya <Priyamadiwalar0007@gmail.com>
2022-02-27 22:35:34 +0530

    Added:uc1-Maximum Computation

--- src/com/blz/generic/Generics.java
+++ src/com/blz/generic/Generics.java
@@ -1,7 +1,39 @@
 package com.blz.generic;
 
-public class Generics {
+public class Generics<T extends Comparable<T>> {
+
+	T toCompare1, toCompare2, toCompare3;
+
+	public Generics(T tocompare1, T tocompare2, T tocompare3) {
+		this.toCompare1 = tocompare1;
+		this.toCompare2 = tocompare2;
+		this.toCompare3 = tocompare3;
+	}
+
+	public void testMaximum() {
+		Generics.findMaximum(this.toCompare1, this.toCompare2, this.toCompare3);
+	}
+
+	public static <T extends Comparable<T>> void findMaximum(T firstNumber, T secondNumber, T thirdNumber) {
+		T maximumNumber;
+
+		if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0) {
+			maximumNumber = firstNumber;
+		} else if (secondNumber.compareTo(firstNumber) > 0 && secondNumber.compareTo(thirdNumber) > 0) {
+			maximumNumber = secondNumber;
+		} else {
+			maximumNumber = thirdNumber;
+		}
+
+		System.out.println("The maximum number is: " + maximumNumber);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcom to Generics");
+		System.out.println(" Welcome To Maximum Computation Of Three Numbers Using Java Generics ");
+
+		Integer firstNumber = 15, secondNumber = 22, thirdNumber = 27;
+
+		new Generics<Integer>(firstNumber, secondNumber, thirdNumber).testMaximum();
+
 	}
 }
